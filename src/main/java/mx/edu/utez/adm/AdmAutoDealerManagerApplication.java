093107c00d0fbd5b95c57288b8775f86416d0ba3
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-11-02 23:09:10 -0600

    [FIRST COMMIT]

---
+++ src/main/java/mx/edu/utez/adm/AdmAutoDealerManagerApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.adm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmAutoDealerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdmAutoDealerManagerApplication.class, args);
+    }
+
+}

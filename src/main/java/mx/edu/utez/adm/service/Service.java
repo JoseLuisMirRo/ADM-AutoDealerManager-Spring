df0c32115b1b7291d521936526d04c4ba4ff6a13
pIEROvEIO <20233tn072@utez.edu.mx>
2024-11-05 11:05:16 -0600

    add service

---
+++ src/main/java/mx/edu/utez/adm/service/Service.java
@@ -0,0 +1,9 @@
+package mx.edu.utez.adm.service;
+
+public class Service {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean isActive;
+}

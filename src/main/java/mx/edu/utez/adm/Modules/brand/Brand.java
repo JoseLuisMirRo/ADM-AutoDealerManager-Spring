6b3b4f88c43f61f0f6f72f026964b3fd8ae88e23
gabyFredy <20233TN189@utez.edu.mx>
2024-11-05 22:23:41 -0600

    [UPDATE] brand

---
+++ src/main/java/mx/edu/utez/adm/Modules/brand/Brand.java
@@ -0,0 +1,18 @@
+package mx.edu.utez.adm.Modules.brand;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "brand")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "createdAt", nullable = false)
+    private String createdAt;
+}

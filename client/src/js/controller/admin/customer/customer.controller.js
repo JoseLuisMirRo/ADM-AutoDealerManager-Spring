13a6956f00e90925141a7e4d1ad482306aac4225
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2024-11-30 22:22:42 -0600

    Customer CRUD Ready

--- client/src/js/controller/admin/customer/customer.controller.js
+++ client/src/js/controller/admin/customer/customer.controller.js
@@ -211,7 +211,7 @@ const loadCustomers = async () => {
             <td>${customer.email}</td>
             <td>${customer.employee.name} ${customer.employee.lastname} ${customer.employee.surname}</td>
             <td>
-            ${employee.status ? `<span class="badge bg-success">Activo</span>` : `<span class="badge bg-danger">Inactivo</span>`}
+            ${customer.status ? `<span class="badge bg-success">Activo</span>` : `<span class="badge bg-danger">Inactivo</span>`}
             </td>
             <td class="text-center">
                 <div class="btn-group" role="group" aria-label="Grupo de botones">
@@ -220,7 +220,7 @@ const loadCustomers = async () => {
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                         </svg>
                     </button>
-                    <button type="button" class="btn btn-outline-warning" onclick="confirmDeleteCustomer(${customer.id})">
+                    <button type="button" class="btn btn-outline-warning" onclick="confirmChangeStatusCustomer(${customer.id},${customer.status})">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
                             <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
                             <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
@@ -233,30 +233,34 @@ const loadCustomers = async () => {
     tbody.innerHTML = content;
 };
 
-// Función que muestra el SweetAlert2 para confirmar la eliminación
-const confirmDeleteCustomer = (id) => {
+// Función que muestra el SweetAlert2 para confirmar el cambio de estado del cliente -> Se puede implementar en los otros controladores
+const confirmChangeStatusCustomer = (id, status) => {
     Swal.fire({
-        title: "¿Estás seguro?",
-        text: "¡No podrás revertir esto!",
+        title: `¿Estás seguro de ${status ? 'deshabilitar' : 'habilitar'} el cliente?`,
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Sí, eliminarlo",
+        confirmButtonText: `${status ? 'Deshabilitar' : 'Habilitar'}`,
         cancelButtonText: "Cancelar"
     }).then(async (result) => {
         if (result.isConfirmed) {
             // Si el usuario confirma, eliminar el cliente
             try {
-                const response = await fetch(`${URL}/adm/customer/${id}`, {
-                    method: 'DELETE',
+                const response = await fetch(`${URL}/adm/customer/status`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'accept': 'application/json'
+                    },
+                    body: JSON.stringify({ id: id, status: !status })
                 });
 
                 if (response.ok) {
                     // Mostrar confirmación de eliminación
                     Swal.fire(
-                        "¡Eliminado!",
-                        "El cliente ha sido eliminado.",
+                        `${status ? 'Deshabilitado' : 'Habilitado'}`,
+                        `El cliente ha sido ${status ? 'deshabilitado' : 'habilitado'} correctamente.`,
                         "success"
                     );
                     // Recargar la tabla de clientes

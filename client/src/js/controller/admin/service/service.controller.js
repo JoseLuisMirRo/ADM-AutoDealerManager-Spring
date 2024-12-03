3c8d67f0b1e0d77ff0c668b3b99afa2ecec77264
gabyFredy <20233TN189@utez.edu.mx>
2024-12-03 12:06:09 -0600

    [UPDETE] creación de sweetAlert

--- client/src/js/controller/admin/service/service.controller.js
+++ client/src/js/controller/admin/service/service.controller.js
@@ -42,7 +42,7 @@ const loadTable = async () => {
                                     <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                 </svg>
                             </button>
-                            <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#changeServiceStatusModal" data-employee-id="${service.id}" data-service-status="${service.status}">
+                            <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#changeServiceStatusModal" data-service-id="${service.id}" data-service-status="${service.status}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
                                     <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
                                     <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
@@ -79,21 +79,48 @@ const saveService = async () => {
         price: parseFloat(document.getElementById('servicePrice').value)
     };
 
-    await fetch(`${URL}/adm/service`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(service)
-    }).then(response => response.json()).then(async response => {
-        console.log(service);
-        console.log(response);
-        service = {};
-        await loadTable();
-        form.reset();
-    }).catch(console.log);
+    Swal.fire({
+        icon: 'question',
+        title: '¿Realizar esta operación?',
+        showConfirmButton: true,
+        showCancelButton: true
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            try {
+                const response = await fetch(`${URL}/adm/service`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify(service)
+                });
+
+                if (!response.ok) throw new Error(`Error al guardar: ${response.statusText}`);
+                await response.json();
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Operación exitosa',
+                    text: 'El servicio se guardó correctamente'
+                });
+
+                // Resetear el formulario y recargar la tabla
+                service = {}; // Limpiar el objeto service
+                await loadTable(); // Recargar la tabla
+                form.reset(); // Resetear el formulario
+            } catch (error) {
+                console.error("Error al guardar el servicio: ", error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error en la operación',
+                    text: error.message
+                });
+            }
+        }
+    });
 };
+
 
 document.getElementById('updateServiceModal').addEventListener('show.bs.modal', async event => {
     const button = event.relatedTarget;
@@ -119,25 +146,53 @@ const updateService = async () => {
         price: parseFloat(document.getElementById('updateServicePrice').value)
     };
 
-    await fetch(`${URL}/adm/service/${service.id}`, {
-        method: 'PUT',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(service)
-    }).then(response => response.json()).then(async response => {
-        console.log(service);
-        console.log(response);
-        service = {};
-        await loadTable();
-        form.reset();
-    }).catch(console.log);
-}
+    Swal.fire({
+        icon: 'question',
+        title: '¿Realizar esta operacion',
+        showConfirmButton: true,
+        showCancelButton: true
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            try {
+                const response = await fetch(`${URL}/adm/service/${service.id}`, {
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify(service)
+                });
+
+                if (!response.ok) throw new Error(`Error al actualizar: ${response.statusText}`);
+                await response.json();
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Operación exitosa',
+                    text: 'La actualización se realizó de manera correcta'
+                });
+
+                //Cierre el modal
+                const modal = document.getElementById('updateServiceModal');
+                const updateServiceModal = bootstrap.Modal.getInstance(modal);
+                updateServiceModal.hide();
+
+                await loadTable();
+            } catch (error) {
+                console.error("Error al actualizar el servicio: ", error);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error en la operación',
+                    text: error.message
+                });
+            }
+        }
+    });
+};
 
 
 // Eliminar un servicio
-const deleteService = async () => {
+const changeEmployeeStatus = async () => {
     await fetch(`${URL}/adm/service/${service.id}`, {
         method: 'DELETE',
         headers: {

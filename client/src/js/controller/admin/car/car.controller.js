6567d5e1fb1612c92092da4b10ac1f0500ac130e
dsanchezz <20213tn039@utez.edu.mx>
2024-12-04 14:21:09 -0600

    [UPDATE] Actualizacion de banner

--- client/src/js/controller/admin/car/car.controller.js
+++ client/src/js/controller/admin/car/car.controller.js
@@ -24,7 +24,6 @@ const loadCards = async()=> {
         content += createCarCard(car); //Agregamos cada tarjeta
         cardContainer.innerHTML = content;
     });
-
 }
 
 const createCarCard = car => {
@@ -40,18 +39,18 @@ const createCarCard = car => {
                             <div class="col text-start m-3 ms-0">
                                 <div class="d-flex justify-content-between">
                                     <div class="btn-group " role="group" aria-label="Grupo de botones">
-                                        <button type="button" class="btn boton-verMas" data-bs-toggle="modal" data-bs-target="#seeMore" data-car-id="${car.id}">
+                                        <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#seeMore" data-car-id="${car.id}">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
                                                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
                                             </svg>
                                         </button>
-                                        <button type="button" class="btn boton-editar" data-bs-toggle="modal" data-bs-target="#updateCar">
+                                        <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#updateCar">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                                             </svg>
                                         </button>
-                                        <button type="button" class="btn boton-eliminar" onclick="confirmDeleteCar(${car.id})">
+                                        <button type="button" class="btn btn-outline-info" onclick="confirmDeleteCar(${car.id})">
 
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
